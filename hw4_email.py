93db02058328d8626fef2a97d22729e73e853995
Darya Belevtseva <dascha067@gmail.com>
2025-10-29 18:25:15 +0300

    domain lists + uniqueness + corporate check (steps 6-8)"

--- hw4_email.py
+++ hw4_email.py
@@ -20,3 +20,36 @@ print(email)
 # 3. Нормализация e-mail адресов: убрать пробелы по краям и привести к нижнему регистру
 email["from"] = email["from"].strip().lower()
 email["to"] = email["to"].strip().lower()
+
+# 4. Извлечь логин и домен из адреса отправителя
+login = email["from"].split("@")[0]
+domain = email["from"].split("@")[1]
+
+# 5. Короткая версия текста: первые 10 символов + "..."
+email["short_body"] = email["body"][:10] + "..."
+
+# Выводим результат для проверки
+print(email["short_body"])
+
+# 6. Создаем списки доменов (личные и корпоративные)
+personal_domains = [
+    'gmail.com', 'list.ru', 'yahoo.com', 'outlook.com', 'hotmail.com',
+    'icloud.com', 'yandex.ru', 'mail.ru', 'list.ru', 'bk.ru', 'inbox.ru'
+]
+corporate_domains = [
+    'company.ru', 'corporation.com', 'university.edu',
+    'organization.org', 'company.org', 'business.net'
+]
+
+# 7. Проверяем, что нет пересечений
+intersection = set(personal_domains) & set(corporate_domains)
+# Выводим результат
+if intersection:
+    raise ValueError(f"Personal and corporate domains intersect: {intersection}")
+
+# 8. # Получаем домен отправителя из email["from"]
+sender_domain = email["from"].split("@")[1]
+#  Булево — отправитель корпоративный
+is_corporate = sender_domain in corporate_domains
+
+print(f"Is corporate sender: {is_corporate}")
